9619e5cdeba5fc355d3da063966a7f238e2b5ad2
Chris Kerr <chriskerr@me.com>
2020-04-15 17:34:25 +1000

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { hydrate, render } from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase/app";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: "penne-pinching.firebaseapp.com",
+	databaseURL: "https://penne-pinching.firebaseio.com",
+	projectId: "penne-pinching",
+	storageBucket: "penne-pinching.appspot.com",
+	messagingSenderId: "305728508038",
+	appId: "1:305728508038:web:ab5a360f93018d34fac107",
+};
+firebase.initializeApp( firebaseConfig );
+
+
+const rootElement = document.getElementById( "root" );
+if ( rootElement.hasChildNodes()) {
+	hydrate( <App />, rootElement );
+} else {
+	render( <App />, rootElement );
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
